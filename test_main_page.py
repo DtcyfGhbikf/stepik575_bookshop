bb34392f8f7e1be3963d1fc85782aeb0893f5109
DtcyfGhbikf <110355517+DtcyfGhbikf@users.noreply.github.com>
2022-08-02 01:07:27 +0300

    add test should_be_login_link

--- test_main_page.py
+++ test_main_page.py
@@ -2,9 +2,14 @@ from selenium.webdriver.common.by import By
 import time
 from .pages.main_page import MainPage
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = MainPage(browser, link)
+    page.open()
     page.go_to_login_page()
-    time.sleep(5)
