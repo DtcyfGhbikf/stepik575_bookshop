800c049d56d3261bf548357f5a5b7069aa431ac1
DtcyfGhbikf <110355517+DtcyfGhbikf@users.noreply.github.com>
2022-08-02 01:38:57 +0300

    added login_page class with locators and asserts and used it in tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 import time
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -10,6 +11,8 @@ def test_guest_should_see_login_link(browser):
 
 def test_guest_can_go_to_login_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/'
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+    main_page = MainPage(browser, link)
+    main_page.open()
+    main_page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
