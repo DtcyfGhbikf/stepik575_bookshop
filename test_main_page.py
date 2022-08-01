240b24a6ce7636e2d31381159722c16ce16b278c
DtcyfGhbikf <110355517+DtcyfGhbikf@users.noreply.github.com>
2022-08-02 02:17:00 +0300

    renamed variable

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,11 @@
-from selenium.webdriver.common.by import By
-import time
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    main_page = MainPage(browser, link)
+    main_page.open()
+    main_page.should_be_login_link()
 
 def test_guest_can_go_to_login_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/'
